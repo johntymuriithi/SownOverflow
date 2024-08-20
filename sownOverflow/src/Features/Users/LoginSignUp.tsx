5dfe46adf271e58d5f8edd32fc6a9f31c927a30c
johntymuriithi <muriithijohn634@gmail.com>
2024-08-20 12:40:52 +0300

    fixed the login unprecidede alert

--- sownOverflow/src/Features/Users/LoginSignUp.tsx
+++ sownOverflow/src/Features/Users/LoginSignUp.tsx
@@ -34,8 +34,6 @@ export default function LoginSignUp() {
     setPostStatus('pending')
     await dispatch(loginUser({email, password})).unwrap()
     navigate('/')
-    e.currentTarget.reset() // this is now working please check it out
-
    } catch (err) {
     console.log(err) // for now only
     alert("Incorrect Email or Password, Try again!!")
@@ -69,6 +67,11 @@ export default function LoginSignUp() {
    }
     // Handle form submission logic here
   };
+
+  const handleFeature = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault()
+    alert("Feature Coming Soon!!")
+  }
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-500">
@@ -154,7 +157,8 @@ export default function LoginSignUp() {
                   <label htmlFor="remember-me" className="ml-2 text-sm text-gray-600">Remember me</label>
                 </div>
                 <div>
-                  <a href="#" className="text-sm text-blue-600 hover:underline">Forgot password?</a>
+                  <a href="#" className="text-sm text-blue-600 hover:underline" 
+                  onClick={handleFeature}>Forgot password?</a>
                 </div>
               </div>
 
@@ -166,19 +170,6 @@ export default function LoginSignUp() {
             </form>
           ) : (
             <form onSubmit={handleSignup} className={`transition-opacity duration-900 ${!isLogin ? 'opacity-100' : 'opacity-0'}`}>
-              {/* <div className="px-6 sm:px-0 max-w-sm">
-              <button type="button" className="text-white w-full bg-[#4285F4] hover:bg-blue-800 transition focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between mr-2 mb-2">
-                <svg className="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
-                Sign up with Google
-                <div></div>
-              </button>
-              </div> */}
-
-              {/* <div className="flex items-center my-6">
-                <span className="flex-grow bg-gray-300 rounded h-0.5"></span>
-                <span className="mx-4 text-sm text-gray-500">or</span>
-                <span className="flex-grow bg-gray-300 rounded h-0.5"></span>
-              </div> */}
 
               <div className="mb-4 relative">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
